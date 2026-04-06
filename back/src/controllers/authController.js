844d390d8cf5435a121dbbdb57884aaac8e1e542
Matt-0201 <matthieukollen@gmail.com>
2026-04-06 22:09:32 +0200

    Signature d'un token jwt à la connexion, manqe le cas d'ajout d'utilisateurs

--- back/src/controllers/authController.js
+++ back/src/controllers/authController.js
@@ -1,3 +1,5 @@
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, "../.env")});
 const jwt = require("jsonwebtoken");
 const { getDB } = require('../config/db');
 
@@ -6,27 +8,62 @@ exports.login = async (req, res) => {
         const { email, password } = req.body;
         const db = getDB();
         const users = db.collection("users");
-        
         let existing_user = await users.findOne({ email: email });
         if (existing_user == null) {
-            console.log("hey")
-            //const userId = req.body; 
-            //const token = jwt.sign({ userId }, process.env.LOGIN_KEY, { expiresIn: "24h" });
-            res.send("Utilsateur inconnu, création d'un nouvel utilisateur nécessaire");
+            // CHANGER : METTRE USER + ROLE + ISCONNECTED DANS LE PAYLOD
+            // ET AJOUTER USER DANS DB
+            const payload = {email: req.body.email};
+            const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "24h" });
+            res.status(200).json({
+                    "success": "true",
+                    "message": "Connexion réussie, utilisateur créé",
+                    "user": req.body.email,
+                    "token": token
+                }) 
         } else {
-            if (existing_user.password === password) {
-                const name = email.split("@");
-                res.send("Bienvenue " + name[0].toString());
+            if (existing_user.password === password) { // Temporaire, comparer avec une version cryptée du mdp
+                const payload = {email: existing_user.email, role: existing_user.role};
+                const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "24h" });
+                res.status(200).json({
+                    "success": "true",
+                    "message": "Connexion réussie, utilisateur retrouvé",
+                    "user": req.body.email,
+                    "token": token
+                })
             } else {
-                res.send("Mot de passe erroné. Veuillez réessayer");
+                res.status(401).json({
+                    "success": "false",
+                    "message": "Mot de passe incorrect"
+                });
             }
         }
     } catch (err) {
-        res.status(500).send("Erreur interne du serveur");
+        res.status(500).json({
+            "success": "false",
+            "message": "Erreur serveur"
+        });
     }
 };
 
 exports.getHome = (req, res) => {
     const path = require('path');
+    // TEMPORAIRE : LIER AVEC LE FRONT EN RENVOYANT UN CERTAIN MESSAGE JSON
     res.sendFile(path.join(__dirname, '../../index.html'));
 };
+
+exports.getUsers = async (req, res) => {
+    const db = getDB();
+    const users = db.collection("users");
+    const listUsers = await users.find({}).toArray();
+    if (req.body.role == "admin") {
+        res.status(200).json({
+            "success": "true",
+            "message": "Utilisateur admin. Possibilité d'accéder à la ressource"
+        });
+    } else {
+        res.status(500).json({
+            "success": "false",
+            "message": "Utilisateur nom admin. Accès à la ressource impossible."
+        })
+    }
+}
--- back/src/routes/authRoutes.js
+++ back/src/routes/authRoutes.js
@@ -8,4 +8,6 @@ router.get('/', authController.getHome);
 // Route POST pour le login
 router.post('/login', authController.login);
 
+router.get('/genoa', authController.getUsers);
+
 module.exports = router;
